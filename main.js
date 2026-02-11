7a4e1a4af2fd249e5582ef66f0bcb0a05a377440
SimoneZoroddu <lenviur@gmail.com>
2026-02-11 15:19:57 +0100

    img pushed by js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // console.log('javascript collegato');
 
+// per allenarmi con questi elementi di js provo a pushare il link dentro l img
+
+const lampImg = document.querySelector('img')
+console.log(lampImg);
+
+lampImg.src = './img/white_lamp.png'
+
