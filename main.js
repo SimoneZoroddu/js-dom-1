ce3523bd1e4c3d840717c9e38503935972df5276
SimoneZoroddu <lenviur@gmail.com>
2026-02-11 15:39:07 +0100

    end of exercise with bonus

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 // console.log('javascript collegato');
 
 // per allenarmi con questi elementi di js provo a pushare il link dentro l img
-
+// mi prendo dall html il tag img con query (che seleziona i tag come se fossi nel css), potei anche dare una classe o un id e prenderlo tramite getElementById (per ID) o getElementsByClassName (per le classi)
 const lampImg = document.querySelector('img')
-console.log(lampImg);
-
+// controllo che funzioni
+//console.log(lampImg);
+// gli cambio l elemento src con l immagine situata nella cartella img lampadina spenta
 lampImg.src = './img/white_lamp.png'
-
+// seleziono il bottone come con l img
 const onOffButton = document.querySelector('button')
-console.log(onOffButton);
+// controllo che funzioni
+//console.log(onOffButton);
+// ci metto un testo dentro il bottone in questo caso 'Accendi'
+onOffButton.innerHTML = 'Accendi'
 
+//rendo il bottone cliccabile grazie al 'click' e gli do una funzione per dirgli cosa deve fare
 onOffButton.addEventListener('click', function () {
-    onOffButton.innerHTML = 'Spegni'
-    lampImg.src = './img/yellow_lamp.png'
+    // gli dico che se il testo all interno del bottone e 'Accendi' cambia img e cambia testo altrimenti l opposto
+    if (onOffButton.innerHTML == 'Accendi') {
+        onOffButton.innerHTML = 'Spegni'
+        lampImg.src = './img/yellow_lamp.png'
+    } else {
+        onOffButton.innerHTML = 'Accendi'
+        lampImg.src = './img/white_lamp.png'
+    }
 })
